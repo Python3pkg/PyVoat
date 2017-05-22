6e7f34cd2cd848040c3a9c6ea1c27ff567aae258
Python3pkg <raliclo@gmail.com>
2017-05-22 11:14:29 -0700

    Convert to Python3

--- example.py
+++ example.py
@@ -9,15 +9,15 @@ subverse = v.getSubverse("api")
 
 #Loop through all unread messages
 for message in messages:
-  print(message.author)
-  print(message.body)
+  print((message.author))
+  print((message.body))
 
-print(user.getInfo())
+print((user.getInfo()))
 
 #Loops through each post in the subverse
 submissions = subverse.getSubmissions()
 for post in submissions:
-  print(post.author)
+  print((post.author))
   comments= post.getComments()
   for comment in comments:
-    print(comment.body)
+    print((comment.body))
